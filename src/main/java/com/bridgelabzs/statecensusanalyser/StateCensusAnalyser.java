7dc1a81a55bcc2d850b475124768664705cfcf01
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-16 19:18:06 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/bridgelabzs/statecensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabzs.statecensusanalyser;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Indian States Census Analyser Problem");
+    }
+}
