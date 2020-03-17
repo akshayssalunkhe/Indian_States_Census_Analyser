aacf6d671ee00d55834a72182b5fe1114b1215d2
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-17 17:11:04 +0530

    ADDED:Test Case To Check Valid File Name If Invalid Should Thorow Exception

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.statecensusanalysertest;
 
 import com.bridgelabzs.statecensusanalyser.StateCensusAnalyser;
+import com.bridgelabzs.statecensusanalyserexception.StateCensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,6 +12,9 @@ public class StateCensusAnalyserTest {
     //GIVING FILE PATH
     private static final String STATE_CENSUS_DATA_PATH = "src/test/resources/StateCensusData.csv";
 
+    //GIVING WRONG FILE PATH
+    private static final String STATE_CENSUS_DATA_WRONG_PATH = "src/test/resources/StateCensusDa.csv";
+
     //CREATING OBJECT
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
@@ -19,6 +23,17 @@ public class StateCensusAnalyserTest {
         try {
             int numberOfRecord = stateCensusAnalyser.loadStateCSVData(STATE_CENSUS_DATA_PATH);
             Assert.assertEquals(29, numberOfRecord);
+        } catch (IOException | StateCensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenStateCensusCSVFile_WhenIncorrect_ShouldReturnCustomException() {
+        try {
+            stateCensusAnalyser.loadStateCSVData(STATE_CENSUS_DATA_WRONG_PATH);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_FOUND, e.type);
         } catch (IOException e) {
             e.printStackTrace();
         }
