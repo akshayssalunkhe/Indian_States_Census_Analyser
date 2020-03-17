d7afbe6617454ab322804f65dd2ae7cab37b2c7f
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-17 17:09:34 +0530

    ADDED:Class To Throw Custom Exception

--- src/main/java/com/bridgelabzs/statecensusanalyserexception/StateCensusAnalyserException.java
+++ src/main/java/com/bridgelabzs/statecensusanalyserexception/StateCensusAnalyserException.java
@@ -1,4 +1,17 @@
 package com.bridgelabzs.statecensusanalyserexception;
 
-public class StateCensusAnalyserException {
+public class StateCensusAnalyserException extends Exception {
+    //CREATING ENUM
+    public enum ExceptionType {
+        NO_SUCH_FILE_FOUND;
+    }
+
+    //CREATING OBJECT
+    public ExceptionType type;
+
+    //CONSTRUCTOR
+    public StateCensusAnalyserException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
 }
