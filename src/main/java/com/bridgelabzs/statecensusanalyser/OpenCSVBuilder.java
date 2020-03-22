5dc3926106a30f2ee20aaf44426fd04b5fb5defe
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-22 22:18:14 +0530

    REFACTORED:Method From Iterator To List

--- src/main/java/com/bridgelabzs/statecensusanalyser/OpenCSVBuilder.java
+++ src/main/java/com/bridgelabzs/statecensusanalyser/OpenCSVBuilder.java
@@ -4,21 +4,19 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
-import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVBuilder implements ICSVBuilder {
-    //GENERIC METHOD TO BUILD THE CSV FILE
     @Override
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+    public <E> List getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException {
         try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
+            CsvToBean csvToBeanBuilder = new CsvToBeanBuilder(reader)
+                    .withType(csvClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBeanBuilder.parse();
         } catch (RuntimeException e) {
             throw new CSVBuilderException(CSVBuilderException.ExceptionType.DELIMITER_OR_HEADER_INCORRECT, "DELIMITER_OR_HEADER_INCORRECT_ERROR_ BUILDING_CSV");
-
         }
     }
 }
