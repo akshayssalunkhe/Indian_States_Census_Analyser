833a6bc888437d4a037037006e3f9c5690451c5e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-22 21:49:46 +0530

    REFACTORED 5:Code From Iteratore To List

--- src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
@@ -1,9 +1,8 @@
 package com.bridgelabzs.statecensusanalyser;
 
 import java.io.Reader;
-import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvLoaderClass) throws CSVBuilderException;
+    public <E> List getCSVFileList(Reader reader, Class<E> csvLoaderClass) throws CSVBuilderException;
 }
-
