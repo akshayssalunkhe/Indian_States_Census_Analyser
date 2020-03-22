79d8cf9cb1486f15e5632dee48ad6950df14f3cf
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-22 19:12:45 +0530

    ADDED:Extended Exception To Method

--- src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
@@ -1,11 +1,9 @@
 package com.bridgelabzs.statecensusanalyser;
 
-import com.bridgelabzs.statecensusanalyserexception.StateCensusAnalyserException;
-
 import java.io.Reader;
 import java.util.Iterator;
 
 public interface ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvLoaderClass) throws StateCensusAnalyserException;
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvLoaderClass) throws CSVBuilderException;
 }
 
