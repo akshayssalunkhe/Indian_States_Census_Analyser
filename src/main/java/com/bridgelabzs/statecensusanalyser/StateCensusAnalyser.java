5ec6ec66ed078c01ea8714a355e00eddee822aa2
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-24 23:04:52 +0530

    ADDED:Method To Sort Census Data By State Name In Alphabetical Order

--- src/main/java/com/bridgelabzs/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabzs/statecensusanalyser/StateCensusAnalyser.java
@@ -2,15 +2,21 @@ package com.bridgelabzs.statecensusanalyser;
 
 import com.bridgelabzs.csvstates.CSVStates;
 import com.bridgelabzs.statecensusanalyserexception.StateCensusAnalyserException;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
 public class StateCensusAnalyser {
+    //CREATING GLOBAL LIST OBJECT
+    List<CSVStateCensus> csvList = null;
+
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("Welcome To Indian States Census Analyser Problem");
     }
@@ -23,7 +29,7 @@ public class StateCensusAnalyser {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_EXTENSION, "NO_SUCH_EXTENSION");
         try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
             ICSVBuilder icsvBuilder = new OpenCSVBuilder();
-            List csvList = icsvBuilder.getCSVFileList(reader, CSVStateCensus.class);
+            csvList = icsvBuilder.getCSVFileList(reader, CSVStateCensus.class);
             return csvList.size();
         } catch (NoSuchFileException e) {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_FOUND, "NO_SUCH_FILE_FOUND");
@@ -39,7 +45,7 @@ public class StateCensusAnalyser {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_EXTENSION, "NO_SUCH_EXTENSION");
         try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
             ICSVBuilder icsvBuilder = new OpenCSVBuilder();
-            List csvList = icsvBuilder.getCSVFileList(reader, CSVStates.class);
+            csvList = icsvBuilder.getCSVFileList(reader, CSVStates.class);
             return csvList.size();
         } catch (NoSuchFileException e) {
             throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_FOUND, "NO_SUCH_FILE_FOUND");
@@ -53,5 +59,30 @@ public class StateCensusAnalyser {
         if (filePath.lastIndexOf(".") != -1 && filePath.lastIndexOf(".") != 0)
             return filePath.substring(filePath.lastIndexOf(".") + 1);
         else return "";
+    }
+
+    //METHOD TO GET STATE WISE SORTED DATA
+    public String getStateWiseSortedCensusData(String csvFilePath) throws StateCensusAnalyserException, IOException, CSVBuilderException {
+        loadStateCSVData(csvFilePath);
+        if (csvList == null || csvList.size() == 0)
+            throw new StateCensusAnalyserException(StateCensusAnalyserException.ExceptionType.NO_SUCH_FILE_FOUND, "NO_SUCH_FILE_FOUND");
+        Comparator<CSVStateCensus> censusComparator = Comparator.comparing(census -> census.state);
+        this.sortCSVCensusData(csvList, censusComparator);
+        String toJson = new Gson().toJson(csvList);
+        return toJson;
+    }
+
+    // GENERIC METHOD TO SORT CENSUS DATA
+    public void sortCSVCensusData(List<CSVStateCensus> csvList, Comparator<CSVStateCensus> censusComparator) {
+        for (int indexOne = 0; indexOne < csvList.size(); indexOne++) {
+            for (int indexTwo = 0; indexTwo < csvList.size() - indexOne - 1; indexTwo++) {
+                CSVStateCensus census1 = csvList.get(indexTwo);
+                CSVStateCensus census2 = csvList.get(indexTwo + 1);
+                if (censusComparator.compare(census1, census2) > 0) {
+                    csvList.set(indexTwo, census2);
+                    csvList.set(indexTwo + 1, census1);
+                }
+            }
+        }
     }
 }
