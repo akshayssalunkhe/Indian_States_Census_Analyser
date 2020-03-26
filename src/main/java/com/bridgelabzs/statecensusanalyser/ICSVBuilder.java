3932a761cb4c655502ca7a35e410fd134f189b04
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-26 15:27:34 +0530

    ADDED:Method Declaration In The Interface For Iterator

--- src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
+++ src/main/java/com/bridgelabzs/statecensusanalyser/ICSVBuilder.java
@@ -1,8 +1,11 @@
 package com.bridgelabzs.statecensusanalyser;
 
 import java.io.Reader;
+import java.util.Iterator;
 import java.util.List;
 
 public interface ICSVBuilder {
     public <E> List getCSVFileList(Reader reader, Class<E> csvLoaderClass) throws CSVBuilderException;
+
+    public <E> Iterator getCSVFileIterator(Reader reader, Class<E> csvStateCensusClass) throws CSVBuilderException;
 }
