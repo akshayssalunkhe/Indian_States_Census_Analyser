89b5bf856f9322891507efe2805aeed5b8ed74b7
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-27 11:52:39 +0530

    ADDED:DAO Class

---
+++ src/main/java/com/bridgelabzs/statecensusanalyser/CensusDAO.java
@@ -0,0 +1,28 @@
+package com.bridgelabzs.statecensusanalyser;
+
+import com.bridgelabzs.csvstates.CSVStates;
+
+public class CensusDAO {
+    public int tin;
+    public String stateName;
+    public int serialNumber;
+    public String state;
+    public int population;
+    public int area;
+    public int density;
+    public String stateCode;
+
+    public CensusDAO(CSVStateCensus csvStateCensus) {
+        this.state = csvStateCensus.state;
+        this.population = csvStateCensus.population;
+        this.area = csvStateCensus.areaInSqKm;
+        this.density = csvStateCensus.densityPerSqKm;
+    }
+
+    public CensusDAO(CSVStates csvStates) {
+        this.serialNumber = csvStates.serialNumber;
+        this.stateName = csvStates.stateName;
+        this.tin = csvStates.tin;
+        this.stateCode = csvStates.stateCode;
+    }
+}
