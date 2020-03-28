c117e77a543723efbe4db5dee1fd487496869b2e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-28 16:50:05 +0530

    ADDED:POJO File For United State Census

---
+++ src/main/java/com/bridgelabzs/statecensusanalyser/CSVUnitedStatesCensus.java
@@ -0,0 +1,16 @@
+package com.bridgelabzs.statecensusanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUnitedStatesCensus {
+    @CsvBindByName(column = " State Id", required = true)
+    public String stateId;
+    @CsvBindByName(column = "State", required = true)
+    public String usState;
+    @CsvBindByName(column = "Population", required = true)
+    public int usPopulation;
+    @CsvBindByName(column = "Total area", required = true)
+    public double totalArea;
+    @CsvBindByName(column = "Population Density", required = true)
+    public double populationDensity;
+}
